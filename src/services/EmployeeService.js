a042e472c410ac023eff24645acdba6f7690c77e
SusanKwamboka <=>
2020-08-07 14:23:55 +0300

    Linked create employee with Rest API

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -7,7 +7,10 @@ const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
     getEmployees(){
         return axios.get(EMPLOYEE_API_BASE_URL );
     }
-
+     
+     createEmployee(employee){
+         return axios.post(EMPLOYEE_API_BASE_URL, employee);
+     }
 }
 
 export default new EmployeeService();
